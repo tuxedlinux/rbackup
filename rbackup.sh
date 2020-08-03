77aa6bb0d3d1eaab08d29caf4bd2d9cdbc87731d
Aaron <60256394+aaron-dot@users.noreply.github.com>
2020-08-03 18:30:44 +0530

    Update rbackup.sh

--- rbackup.sh
+++ rbackup.sh
@@ -10,7 +10,7 @@
 
 #If you have any other folder or files, add them too :)
 
-#If you want to exclude any files/folders, add a # in front of the sentence.
+#If you want to exclude any files/folders, add a # in front of the sentence and the following line.
 
 #At last, to make this script to work, please make it execuable. Type in: chmod +x ./home/your-username/Path/to/backup-all-your-files.sh. Alter "/Path/to" with your folder.
 
@@ -22,20 +22,34 @@ mkdir /media/$USER/edit-me/All-your-backup
 
 rsync -r /home/$USER/Documents /media/$USER/edit-me/All-your-backup
 
+echo Documents folder has been backed up.
+
 rsync -r /home/$USER/Downloads /media/$USER/edit-me/All-your-backup
+
+echo Downloads folder has been backed up.
 
 rsync -r /home/$USER/Music/ /media/$USER/edit-me/All-your-backup
 
+echo Music folder has been backed up.
+
 rsync -r /home/$USER/Pictures /media/$USER/edit-me/All-your-backup
+
+echo Pictures folder has been backed up.
 
 rsync -r /home/$USER/Public /media/$USER/edit-me/All-your-backup
 
+echo Public folder has been backed up.
+
 rsync -r /home/$USER/Templates /media/$USER/edit-me/All-your-backup
 
-rsync -r /home/$USER/Videos /media/$USER/edit-me/All-your-backup
+echo Templates folder has been backed up.
 
 rsync -r /home/$USER/Videos /media/$USER/edit-me/All-your-backup
+
+echo Videos folder has been backed up.
 
 rsync -r /home/$USER/Desktop /media/$USER/edit-me/All-your-backup
+
+echo Desktop has been backed up.
 
 echo Backup Done.
